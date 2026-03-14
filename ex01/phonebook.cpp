4bc3e889957e1f898a303ccb7039a9b71ddd80fc
itu-itis24-taylan23 <taylan23@itu.edu.tr>
2026-03-14 14:29:36 +0300

    türkçe karakterler +  enter

--- ex01/phonebook.cpp
+++ ex01/phonebook.cpp
@@ -1,92 +1,72 @@
 #include "phonebook.hpp"
 
-void Contact::setFirstName(const std::string& firstName){
-	this->firstName = firstName;
-};
-void Contact::setLastName(const std::string& lastName){
-	this->lastName = lastName;
-};
-void Contact::setNickname(const std::string& nickname){
-	this->nickname = nickname;
-};
-void Contact::setPhoneNumber(const std::string& phoneNumber){
-	this->phoneNumber = phoneNumber;
-};
-void Contact::setDarkestSecret(const std::string& darkestSecret){
-	this->darkestSecret = darkestSecret;
-};
-std::string Contact::getfirstName() const{
-	return(firstName);
-};
-std::string Contact::getLastName() const{
-	return(lastName);
-};
-std::string Contact::getNickname() const{
-	return(nickname);
-};
-std::string Contact::getPhoneNumber() const{
-	return(phoneNumber);
-};
-std::string Contact::getDarkestSecret() const{
-	return(darkestSecret);
-};
-
-std::string Contact::formatField(std::string content) const{
-    if (content.length() > 10)
-       return(content.substr(0, 9) + '.');
-    return content;
-}
-
-void Contact::printIndex(int index) const{
-    if (this->firstName.empty())
-        return; 
-    std::cout << "|" << std::setw(10) << index;
-    std::cout << "|" << std::setw(10) << formatField(this->firstName);
-    std::cout << "|" << std::setw(10) << formatField(this->lastName);
-    std::cout << "|" << std::setw(10) << formatField(this->nickname) << "|" << std::endl;
-}
-
-void Contact::printFull() const{
-    if (this->firstName.empty())
-        return; 
-    std::cout << "First Name:" << this->firstName << std::endl;
-    std::cout << "Last Name:" << this->lastName << std::endl;
-    std::cout << "Nickname:" <<this->nickname << std::endl;
-    std::cout << "Phone Number:" <<this->phoneNumber << std::endl;
-    std::cout << "Darkest Secret:" <<this->darkestSecret << std::endl;
-}
-
 PhoneBook::PhoneBook() {
-    this->current_idx = 0;
+	this->current_idx = 0;
 }
 
 void PhoneBook::addContact()
 {
-	Contact& newContact = contacts[current_idx % 8];
-	std::string input;
+    Contact& newContact = contacts[current_idx % 8];
+    std::string input;
 
-	std::cout << "First Name: ";
-	std::getline(std::cin>>std::ws, input);
-	newContact.setFirstName(input);
+    input = "";
+    while (input.empty()) {
+        std::cout << "First Name: ";
+        if (!std::getline(std::cin, input))
+			return;
+    }
+    newContact.setFirstName(input);
 
-	std::cout << "Last Name: ";
-	std::getline(std::cin>>std::ws, input);
-	newContact.setLastName(input);
+    input = "";
+    while (input.empty()) {
+        std::cout << "Last Name: ";
+        if (!std::getline(std::cin, input))
+			return;
+    }
+    newContact.setLastName(input);
 
-	std::cout << "Nickname: ";
-	std::getline(std::cin>>std::ws, input);
-	newContact.setNickname(input);
+    input = "";
+    while (input.empty()) {
+        std::cout << "Nickname: ";
+        if (!std::getline(std::cin, input)) 
+			return;
+    }
+    newContact.setNickname(input);
 
-	std::cout << "Phone Number: ";
-	std::getline(std::cin>>std::ws, input);
-	newContact.setPhoneNumber(input);
+    input = "";
+    while (true) {
+        std::cout << "Phone Number: ";
+        if (!std::getline(std::cin, input)) 
+            return;
 
-	std::cout << "Darkest Secret: ";
-	std::getline(std::cin>>std::ws, input);
-	newContact.setDarkestSecret(input);
+        if (input.empty())
+            continue;
 
-	current_idx++;
+        bool only_digits = true;
+        for (size_t i = 0; i < input.length(); i++) {
+            if (!std::isdigit(input[i])) {
+                only_digits = false;
+                break;
+            }
+        }
 
+        if (only_digits)
+            break;
+        else
+            std::cout << "Error: The phone number must consist only of digits!" << std::endl;
+    }
+    newContact.setPhoneNumber(input);
+
+    input = "";
+    while (input.empty()) {
+        std::cout << "Darkest Secret: ";
+        if (!std::getline(std::cin, input)) 
+			return;
+    }
+    newContact.setDarkestSecret(input);
+
+    std::cout << "Contact added to index " << current_idx % 8 << std::endl;
+    current_idx++;
 }
 
 void PhoneBook::searchContact()
@@ -112,6 +92,7 @@ void PhoneBook::searchContact()
 		std::cout << "Invalid index!" << std::endl;
 }
 
+
 int main() {
     PhoneBook myPhoneBook;
     std::string command;
@@ -119,13 +100,15 @@ int main() {
 
     while (true) {
         std::cout << "Komut girin (ADD, SEARCH, EXIT): ";
+
         if (!(std::cin >> command) || command == "EXIT")
             break;
-        
+		std::cin.ignore(1000, '\n');
         if (command == "ADD") {
 			myPhoneBook.addContact();
         } else if (command == "SEARCH") {
             myPhoneBook.searchContact();
+			std::cin.ignore(1000, '\n'); 
         }
     }
     return 0;
--- ex01/phonebook.hpp
+++ ex01/phonebook.hpp
@@ -1,31 +1,8 @@
+#include "contact.hpp"
 #include <string>
 #include <iostream>
 #include <iomanip>
-
-class Contact
-{
-	private:
-		std::string firstName;
-		std::string lastName;
-		std::string nickname;
-		std::string phoneNumber;
-		std::string darkestSecret;
-		std::string formatField(std::string content) const;
-
-	public:
-		void setFirstName(const std::string& firstName);
-		void setLastName(const std::string& lastName);
-		void setNickname(const std::string& nickname);
-		void setPhoneNumber(const std::string& phoneNumber);
-		void setDarkestSecret(const std::string& darkestSecret);
-		std::string getfirstName() const;
-		std::string getLastName() const;
-		std::string getNickname() const;
-		std::string getPhoneNumber() const;
-		std::string getDarkestSecret() const;
-		void printIndex(int index) const;
-		void printFull() const;
-};
+#include <cctype>
 
 class PhoneBook
 {
